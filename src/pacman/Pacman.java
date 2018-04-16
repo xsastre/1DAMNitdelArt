5c298618ffe5782c9323752d5c095434e8962307
administrator <administrator@W8LAB>
2018-04-16 16:50:38 +0200

    Modificació del codi base del pacman. He pujat la versió JFrame

---
+++ src/pacman/Pacman.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacman;
+
+/**
+ *
+ * @author xavier
+ */
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates por andres2288 javayotros.blogspot.com
+ * and open the template in the editor.
+ */
+
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new tablero());
+        setTitle("P A C M A N");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 420);
+        setLocationRelativeTo(null);
+        setVisible(true);        
+    }
+
+    public static void main(String[] args) {
+
+          EventQueue.invokeLater(() -> {
+            Pacman ex = new Pacman();
+            ex.setVisible(true);
+        });
+
+    }
+}
